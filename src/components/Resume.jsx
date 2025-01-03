feb8b8ef467d0d3869700a5c2abd90bfa862f4f8
SMARAPD - Bruno Empke <BEMPKE@SMARAPD.COM.BR>
2025-01-03 11:26:41 -0300

    Add more texts in resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -10,32 +10,17 @@ export function Resume() {
                 <div className="flex flex-col gap-[30px]" id="resume">
                     <div>
                         <h4 className="text-[#f4f4f4] text-[2.4rem] font-[600] pb-4">
-                            <Cover>Freelancer</Cover>
-                        </h4>
-                        <span className="text-blue-400 my-[10px]">
-                            April 2019 - At the moment
-                        </span>
-                        <p
-                            className="text-neutral-800 max-w-[400px] mt-2 dark:text-neutral-200 text-xs md:text-lg font-normal mb-8">
-                            I using my skills for develop web and mobile aplcations focused in a bussines solutions like a financial administrative and helthy activities.
-
-                            At the moment i contribuit for develop a app for help people with a diabets for control your day and your mediciines with a insights and dashboards for growth your helthy and a life quality.
-                        </p>
-                    </div>
-                    <div>
-                        <h4 className="text-[#f4f4f4] text-[2.4rem] font-[600] pb-4">
                             <Cover>SMARAPD</Cover>
                         </h4>
                         <span className="text-blue-400 my-[10px]">
-                            May 2023 - May 2024
+                            Sep 2023 - at the moment
                         </span>
                         <p
                             className="text-neutral-800 max-w-[400px] mt-2 dark:text-neutral-200 text-xs md:text-lg font-normal mb-8">
                             I lead the automation project from scratch with Cypress for automated tests, initially regression tests and smoke tests, I help other QA's on the team with questions and suggestions about automation.
+                            <p className="text-neutral-800 max-w-[400px] mt-2 dark:text-neutral-200 text-xs md:text-lg font-normal mb-8">Project also aims to implement CI/CD on an automated conveyor belt, carrying out tests in headless mode and cross-browser for greater coverage.</p>
 
-                            Project also aims to implement CI/CD on an automated conveyor belt, carrying out tests in headless mode and cross-browser for greater coverage.
-
-                            Mapping requirements for automated tests in the main routines of the WEB system.
+                            <p className="text-neutral-800 max-w-[400px] mt-2 dark:text-neutral-200 text-xs md:text-lg font-normal mb-8">Mapping requirements for automated tests in the main routines of the WEB system.</p>
                         </p>
                     </div>
                     <div>
@@ -80,6 +65,77 @@ export function Resume() {
                             className="text-neutral-800 max-w-[400px] mt-2 dark:text-neutral-200 text-xs md:text-lg font-normal mb-8">
                             System analysis and development, Systems Engineering
                         </p>
+                    </div>
+                </div>
+            ),
+        },
+        {
+            title: "Volunteer",
+            content: (
+                <div>
+                    <div>
+                        <h4 className="text-[#f4f4f4] text-[2.4rem] font-[600] pb-4">
+                            <Cover>Lacrei Saúde</Cover>
+                        </h4>
+                        <span className="text-blue-400 my-[10px]">
+                            May 2024 - Jun 2024
+                        </span>
+                        <p
+                            className="text-neutral-800 max-w-[400px] mt-2 dark:text-neutral-200 text-xs md:text-lg font-normal mb-8">
+                            Along with QA's time, I implemented a test automation flow via Github Actions, reviewing, updating and creating automation scripts in Cypress that validate all main and secondary routines of the system from end to end whenever a pull request is made and/or a push to the main branch.
+                            <p className="text-neutral-800 max-w-[400px] mt-2 dark:text-neutral-200 text-xs md:text-lg font-normal mb-8">This automation has been renewed in both the production environment and the approval environment.</p>
+
+
+                            <p className="text-neutral-800 max-w-[400px] mt-2 dark:text-neutral-200 text-xs md:text-lg font-normal mb-8">Use libraries such as (cypress-xpath, cypress-mailosaur, cypress-slow-down and @faker-js/faker) to facilitate and increase test coverage. I also used the Page Objects Model concept in Cypress to separate all system endpoints (URL) into two JSON files, one for the production environment and the other for Approval.</p>
+
+
+                            <p className="text-neutral-800 max-w-[400px] mt-2 dark:text-neutral-200 text-xs md:text-lg font-normal mb-8">We are able to implement an effective CI/CD that detects failures and configurations before the customer requests us, making the process proactive and no longer reactive.</p>
+
+
+                            <p className="text-neutral-800 max-w-[400px] mt-2 dark:text-neutral-200 text-xs md:text-lg font-normal mb-8"> Technologies, tools and Frameworks used: Scrum, Kanban, Agile Methodologies, Javascript, Cypress and NodeJs.</p>
+
+
+                        </p>
+                    </div>
+                    <div>
+                        <h4 className="text-[#f4f4f4] text-[2.4rem] font-[600] pb-4">
+                            <Cover>Pipoca Ágil</Cover>
+                        </h4>
+                        <span className="text-blue-400 my-[10px]">
+                            Dec 2024 - at the moment
+                        </span>
+                        <p className="text-neutral-800 max-w-[400px] mt-4 dark:text-neutral-200 text-xs md:text-lg font-normal mb-2">Key responsability:</p>
+                        <p
+                            className="text-neutral-800 max-w-[400px] mt-2 dark:text-neutral-200 text-xs md:text-lg font-normal mb-8">
+                            - Is follow the figma design and create a solutions for get more features for IOS and Android users by using a React Native framework, for give the users a new way to control his day. And i'am a leader for the mobile app development.
+                        </p>
+
+                        <p className="text-neutral-800 max-w-[400px] dark:text-neutral-200 text-xs md:text-lg font-normal mb-2">
+                            Our goal:
+                        </p>
+                        <p className="text-neutral-800 max-w-[400px] mt-2 dark:text-neutral-200 text-xs md:text-lg font-normal mb-8">
+
+                            - Develop a app for help a people have a diabets for control our medicines and get more helthy with a dashboards and recommendations for growth your routine with a system for control your diabets.
+                        </p>
+                        <p className="text-neutral-800 max-w-[400px] dark:text-neutral-200 text-xs md:text-lg font-normal mb-2">Version control and colaboration:</p>
+                        <p className="text-neutral-800 max-w-[400px] mt-2 dark:text-neutral-200 text-xs md:text-lg font-normal mb-8">
+
+                            - Worked with Git and GitHub, following the Gitflow workflow to efficiently manage code versioning and collaborate with other team members.
+                        </p>
+                        <p className="text-neutral-800 max-w-[400px] dark:text-neutral-200 text-xs md:text-lg font-normal mb-2">Agile Methodologies:</p>
+                        <p className="text-neutral-800 max-w-[400px] mt-2 dark:text-neutral-200 text-xs md:text-lg font-normal mb-8">
+
+                            - Actively participated in agile methodologies, such as Scrum and Kanban, to ensure iterative deliveries and continuous alignment with the needs of the development community.
+                        </p>
+                        <p className="text-neutral-800 max-w-[400px] dark:text-neutral-200 text-xs md:text-lg font-normal mb-2">Code:</p>
+                        <p className="text-neutral-800 max-w-[400px] mt-2 dark:text-neutral-200 text-xs md:text-lg font-normal mb-8">
+
+                            - We using a React Native with Expo and Firebase for build this App from scratch to MVP.
+                        </p>
+
+
+                        This app develop using a: React Native, Typescript, Expo, Scrum, Kanban, Jira, Tailwind CSS and Firebase
+
                     </div>
                 </div>
             ),
